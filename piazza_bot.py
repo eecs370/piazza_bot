626a69c3b6dd8ddaf2638ec493842ec39760f9bf
Jack Hong <46696737+jackphong@users.noreply.github.com>
2022-08-01 15:15:55 -0700

    Create piazza_bot.py

---
+++ piazza_bot.py
@@ -0,0 +1,19 @@
+import os
+import sys
+
+from piazza_api.piazza import PiazzaRPC
+
+class PiazzaBot(object):
+    
+    def __init__(self, class_id):
+        self.rpc = PiazzaRPC(class_id)
+        self.rpc.user_login(os.getenv("PIAZZA_EMAIL"), os.getenv("PIAZZA_PASSWORD"))
+        self.network_id = class_id
+
+    def make_note(self, subject, markdown_content, folder):
+        params = {"nid":self.network_id,"type":"note","subject":subject,"content":markdown_content,"anonymous":"no","client_time":"5/29/00 5:29 PM","status":"active","editor":"md","folders":[folder],"config":{}}
+        self.rpc.content_create(params)
+       
+if __name__ == '__main__':    
+    poster = PiazzaBot(os.getenv("PIAZZA_CLASS_ID"))
+    poster.make_note(os.getenv("TITLE"), os.getenv("BODY"), os.getenv("FOLDER")
